5c9632236bd7e6349f01844b98642916f5f3c455
student21 <student21@itpark.ru>
2017-12-01 18:56:06 +0300

    Added conditions

--- Brackets/src/ru/itpark/Main.java
+++ Brackets/src/ru/itpark/Main.java
@@ -2,11 +2,40 @@ package ru.itpark;
 
 public class Main {
 
-    public static void main(String[] args) {
-	  Stack stack = new Stack(10);
-	  String str = "(){}{()}";
-	  String s1 = String.valueOf(str.charAt(0));
-	  stack.push("(");
-      System.out.println(stack.pop());
+  public static void main(String[] args) {
+    Stack stack = new Stack(10);
+    String str = "(){}{()}";
+    for (int i = 0; i < str.length(); i++) {
+      String s1 = String.valueOf(str.charAt(i));
+      if (s1.equals("(") || s1.equals("{") || s1.equals("[")) {
+        stack.push(s1);
+      } else {
+        String bracketFromStack = stack.pop();
+        if (bracketFromStack.equals("(")){
+          if (s1.equals(")")){
+            //норм
+          } else {
+            //не норм
+          }
+        }
+        if (bracketFromStack.equals("{")){
+          if (s1.equals("}")){
+            //норм
+          } else {
+            //не норм
+          }
+        }
+        if (bracketFromStack.equals("[")){
+          if (s1.equals("]")){
+            //норм
+          } else {
+            //не норм
+          }
+        }
+      }
     }
+
+    stack.push("(");
+    System.out.println(stack.pop());
+  }
 }
