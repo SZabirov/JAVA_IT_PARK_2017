0ab2c0bbd2ecfcc370a8af4db805df008b2bb762
student21 <student21@itpark.ru>
2017-12-26 19:27:46 +0300

    Working

---
+++ WebApp/src/ru/itpark/HelloServlet.java
@@ -0,0 +1,20 @@
+package ru.itpark;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class HelloServlet extends HttpServlet {
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    Writer writer = resp.getWriter();
+    writer.write("<html>\n" +
+        "<body>\n" +
+        "<h1>Hello, dear user!</h1>\n" +
+        "</body>\n" +
+        "</html>\n");
+  }
+}
