2c2c5e5f0c553134f199b20eca4c59b2750bd600
SZabirov <zab.sal42@gmail.com>
2018-03-16 18:08:21 +0300

    ftl added

--- carshowroom/src/main/java/ru/itpark/carsshowroom/HelloController.java
+++ carshowroom/src/main/java/ru/itpark/carsshowroom/HelloController.java
@@ -1,6 +1,8 @@
 package ru.itpark.carsshowroom;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class HelloController {
   @Autowired
   CarRepository carRepository;
 
+  @RequestMapping("/")
+  public String index() {
+    return "index";
+  }
+
   @RequestMapping("/hello")
-  public List<Car> hello() {
+  public List<Car> hello(Model model) {
     List<Car> list = (List<Car>) carRepository.findAll();
     return list;
   }
